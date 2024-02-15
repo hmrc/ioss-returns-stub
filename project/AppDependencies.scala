ce2c8c402c7f2cc6936350f6b292c2381c17fc98
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-02-15 13:30:28 +0000

    VEIOSS-540 | Upgrade to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,19 +5,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.23.0"
+  private val bootstrapVersion = "8.4.0"
 
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "com.github.java-json-tools" % "json-schema-validator" % "2.2.14"
+    "uk.gov.hmrc"                 %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "com.github.java-json-tools"  % "json-schema-validator"       % "2.2.14"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % "test, it",
-    "org.scalatest" %% "scalatest" % "3.2.12" % Test,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28" % bootstrapVersion      % "test, it",
+    "org.scalatest"           %% "scalatest"              % "3.2.15"              % Test,
+    "org.playframework"       %% "play-test"              % PlayVersion.current   % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.6"              % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"     % "5.1.0"               % "test, it"
   )
 }
