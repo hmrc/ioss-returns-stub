08fa51cd4d6a35cdeb8ae962cf77a4e5be77cf98
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-12-10 09:12:10 +0000

    VEIOSS-728 | Update to Scala3

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -14,7 +14,7 @@ object CodeCoverageSettings {
     "testOnlyDoNotUseInAppConf.*"
   )
 
-  val settings: Seq[Setting[_]] = Seq(
+  val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
     ScoverageKeys.coverageMinimumStmtTotal := 78,
     ScoverageKeys.coverageFailOnMinimum := true,
