b06bdebf52ae4d3c1a8dd1ed3c66b52f5dd66ac8
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2026-06-25 09:06:33 +0100

    VEI-891 | Address vulnerabilities

--- app/uk/gov/hmrc/iossreturnsstub/utils/JsonSchemaHelper.scala
+++ app/uk/gov/hmrc/iossreturnsstub/utils/JsonSchemaHelper.scala
@@ -34,8 +34,7 @@ package uk.gov.hmrc.iossreturnsstub.utils
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
-import com.github.fge.jsonschema.core.report.ProcessingReport
-import com.github.fge.jsonschema.main.{JsonSchema, JsonSchemaFactory}
+import com.networknt.schema.*
 import play.api.Logging
 import play.api.libs.json.{Json, JsValue}
 import play.api.mvc._
@@ -59,10 +58,11 @@ class JsonSchemaHelper @Inject()(clock: Clock) extends Logging {
     val factory = schemaMapper.getFactory
     val schemaParser: JsonParser = factory.createParser(requestSchema.toString)
     val schemaJson: JsonNode = schemaMapper.readTree(schemaParser)
-    JsonSchemaFactory.byDefault().getJsonSchema(schemaJson)
+    val jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersionDetector.detect(schemaJson))
+    jsonSchemaFactory.getSchema(schemaJson)
   }
 
-  private def validRequest(jsonSchema: JsValue, json: Option[JsValue]): Option[ProcessingReport] = {
+  private def validRequest(jsonSchema: JsValue, json: Option[JsValue]): Option[java.util.Set[ValidationMessage]] = {
     json.map { response =>
       val jsonParser = jsonFactory.createParser(response.toString)
       val jsonNode: JsonNode = jsonMapper.readTree(jsonParser)
@@ -75,9 +75,9 @@ class JsonSchemaHelper @Inject()(clock: Clock) extends Logging {
       case Success(schema) =>
         val validationResult = validRequest(schema, jsonBody)
         validationResult match {
-            case Some(res) if res.isSuccess => f
+            case Some(res) if res.isEmpty => f
             case Some(res) =>
-              logger.error(s"Failed json schema ${res.getExceptionThreshold}")
+              logger.error(s"Failed json schema ${res.size} error(s)")
               res.forEach { test =>
                 logger.error(s"${test.toString}")
                 logger.error(test.getMessage)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.7.0"
 
 
   val compile = Seq(
-    "uk.gov.hmrc"                 %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "com.github.java-json-tools"  % "json-schema-validator"       % "2.2.14"
+    "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "com.networknt"                 %  "json-schema-validator"      % "1.5.9",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.1"
   )
 
   val test = Seq(
